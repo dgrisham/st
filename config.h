509b4e2c887e0dd95564d57d4f15011184d35afd
dgrisham <dmgrisham@gmail.com>
2021-10-17 15:30:35 -0600

    update font to inconsolata

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Inconsolata Nerd Font:style=Regular:antialias=true:size=11";
 static int borderpx = 2;
 
 /*
